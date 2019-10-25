91b311150df2f1e982a0b8f6c65dde020badcddc
lemon24 <damian.adrian24@gmail.com>
2019-10-25 23:34:48 +0300

    Bokeh experimentation.

--- src/smalltsdb/app/__init__.py
+++ src/smalltsdb/app/__init__.py
@@ -1,8 +1,9 @@
+import itertools
 import sqlite3
 
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource
-from bokeh.palettes import RdYlBu4
+from bokeh.palettes import Category10_10 as palette
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.resources import INLINE
@@ -47,31 +48,45 @@ def close_db(error):
 
 
 def graph_p50(tsdb):
-    p = figure(x_axis_type='datetime', title='p50')
+    plot = figure(
+        x_axis_type='datetime',
+        title='p50',
+        plot_width=600,
+        plot_height=200,
+        toolbar_location='above',
+    )
+
+    from bokeh.models import BoxZoomTool
+
+    wzoom = BoxZoomTool(dimensions="width")
+    plot.add_tools(wzoom)
+
+    plot.toolbar.autohide = True
+    # plot.toolbar.active_drag = wzoom # bad for mobile
+    # TODO: also active scroll = zoom horizontal
+
+    # plot.xaxis.axis_label = "time, I guess; is this really needed?"
+    plot.yaxis.axis_label = "whatever"
 
     def metric_to_dict(name):
         pairs = tsdb.get_metric(name, 'tensecond', 'p50', (0, 100))
         lists = list(zip(*pairs))
         return {'timestamp': lists[0], 'values': lists[1]}
 
-    p.line(
-        x='timestamp',
-        y='values',
-        source=metric_to_dict('one'),
-        legend='one',
-        line_width=2,
-        line_color=RdYlBu4[0],
-    )
-    p.line(
-        x='timestamp',
-        y='values',
-        source=metric_to_dict('two'),
-        legend='two',
-        line_width=2,
-        line_color=RdYlBu4[1],
-    )
+    colors = itertools.cycle(palette)
+    names = ['one', 'two']
 
-    return p
+    for name, color in zip(names, colors):
+        plot.line(
+            x='timestamp',
+            y='values',
+            source=metric_to_dict(name),
+            legend=name,
+            line_width=1.2,
+            line_color=color,
+        )
+
+    return plot
 
 
 @blueprint.route('/')
